ec09be8dbe0c84ad2deb684fbf4579b885ef9f2e
Reuben Abdul <reu12th@gmail.com>
2024-02-08 05:50:34 +0100

    14. Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+# 101-stats.py
 """Reads from standard input and computes metrics.
-
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
@@ -10,7 +10,6 @@ prints the following statistics:
 
 def print_stats(size, status_codes):
     """Print accumulated metrics.
-
     Args:
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
@@ -57,3 +56,4 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print_stats(size, status_codes)
         raise
+    
