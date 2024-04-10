433cceb418f194fb5996d6ed6b9693b2d8803757
Reuben Abdul <reu12th@gmail.com>
2024-04-10 06:00:58 +0100

    .

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+/* 100-map.js */
+exports.converter = function (base);
